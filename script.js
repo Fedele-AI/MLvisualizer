c54c309a6d0fabd8fc1441a1fd6b39b76626bd3e
Jenkins, Kenneth Alexander <kjenkins60@gatech.edu>
2025-11-04 12:04:07 -0500

    Added librejs support
    Signed-off-by: Jenkins, Kenneth Alexander <kjenkins60@gatech.edu>

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+// @license magnet:?xt=urn:btih:1f739d935676111cfff4b4693e3816e664797050&dn=gpl-3.0.txt GPL-v3-or-Later
+/**
+ * ML Visualizer - Interactive demonstrations of AI and machine learning architectures
+ * Copyright (C) 2024 Kenneth (Alex) Jenkins
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 // ====== Utility Functions ======
 // Debounce function for resize events
 function debounce(func, wait) {
@@ -3683,3 +3702,5 @@ function animateCUDA() {
         requestAnimationFrame(animateCUDA);
     }, 1000 / state.cuda.speed);
 }
+
+// @license-end
