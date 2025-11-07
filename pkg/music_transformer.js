5dfb0a0434049acf0eb83e2d40cd3ab5eee55b9a
Jenkins, Kenneth Alexander <kjenkins60@gatech.edu>
2025-11-06 23:48:47 -0500

    Fix: librejs support round 2
    Signed-off-by: Jenkins, Kenneth Alexander <kjenkins60@gatech.edu>

--- pkg/music_transformer.js
+++ pkg/music_transformer.js
@@ -1,7 +1,7 @@
 // @license magnet:?xt=urn:btih:1f739d935676111cfff4b4693e3816e664797050&dn=gpl-3.0.txt GPL-3.0
 /**
  * Music Transformer - AI-powered music generation (wasm bindings)
- * Copyright (C) 2024 Kenneth (Alex) Jenkins
+ * Copyright (C) 2025 Kenneth (Alex) Jenkins
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
